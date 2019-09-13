028b804b94c36ed0ae733f8bb9c613331a897fc6
Abit_We <15695203200@163.com>
2019-09-13 23:16:04 +0800

    20190913
    20190913第一次提交

---
+++ workspace_ds/src/com/company/ch1/DuLinkTable/DuNode.java
@@ -0,0 +1,49 @@
+package com.company.ch1.DuLinkTable;
+
+public class DuNode {
+    public Object data;
+    public DuNode next;
+    public DuNode prior;
+
+    public DuNode(){
+        this.data = null;
+        this.next = null;
+        this.prior = null;
+    }
+
+    public DuNode(Object data){
+        this.data = data;
+        this.prior  = null;
+        this.next = null;
+    }
+
+    public DuNode(Object data,DuNode next,DuNode prior){
+        this.data = data;
+        this.next = next;
+        this.prior = prior;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public DuNode getNext() {
+        return next;
+    }
+
+    public void setNext(DuNode next) {
+        this.next = next;
+    }
+
+    public DuNode getPrior() {
+        return prior;
+    }
+
+    public void setPrior(DuNode prior) {
+        this.prior = prior;
+    }
+}
