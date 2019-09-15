65bb7d247cdab30253d9ed8c65985997fd2d0888
AbitGo <15695203200@163.com>
2019-09-15 22:58:56 +0800

    20190915_1
    20190915第一次提交

---
+++ workspace_ds/src/com/company/ch3/LinkStack/LinkStack.java
@@ -0,0 +1,58 @@
+package com.company.ch3.LinkStack;
+
+import com.company.ch2.LinkTable.Node;
+import com.company.ch3.Stack.IStack;
+
+public class LinkStack implements IStack {
+
+    //栈顶元素
+    private Node top;
+    @Override
+    public void clear() {
+        this.top = null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.top == null;
+    }
+
+    @Override
+    public int length() {
+        int len = 0;
+        Node  p = this.top;
+        while(p!=null){
+            ++len;
+            p = p.next;
+        }
+        return len;
+    }
+
+    @Override
+    public Object peek() {
+        if(!isEmpty()){
+            return top.data;
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean push(Object x) throws Exception {
+        this.top = 
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    public void display(){
+        Node p = top;
+        while (p != null) {
+            System.out.println(p.data+" ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+}
