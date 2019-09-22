793160bd52935955d6be35cbae64ebde0010cff7
AbitGo <15695203200@163.com>
2019-09-22 21:47:07 +0800

    20190922
    2019年09月22日第1次提交

--- workspace_ds/src/com/company/ch3/Stack/SqStack/Exp3_1.java
+++ workspace_ds/src/com/company/ch3/Stack/SqStack/Exp3_1.java
@@ -34,44 +34,44 @@ public class Exp3_1 {
                 char c = str.charAt(i);
                 String temp = String.valueOf(c);
                 if (i != len) {
-                    if ((c == '/' && '*' == str.charAt(i + 1)) || (c == '*' && '/' == str.charAt(i + 1))){
-                        temp = temp.concat(String.valueOf(str.charAt(i+1)));
+                    if ((c == '/' && '*' == str.charAt(i + 1)) || (c == '*' && '/' == str.charAt(i + 1))) {
+                        temp = temp.concat(String.valueOf(str.charAt(i + 1)));
                         ++i;
                     }
                 }
-                if(LEFT==verifyFlag(temp)){
+                if (LEFT == verifyFlag(temp)) {
                     sqStack.push(temp);
-                }else if(RIGHT==verifyFlag(temp)){
-                    if(sqStack.isEmpty() || !matches(sqStack.pop().toString(),temp)){
+                } else if (RIGHT == verifyFlag(temp)) {
+                    if (sqStack.isEmpty() || !matches(sqStack.pop().toString(), temp)) {
                         System.out.println("Java语法不合法");
                         return false;
                     }
                 }
             }
             //当栈不为空的时候就证明还有 没有被抵消的符号
-            if(!sqStack.isEmpty()){
+            if (!sqStack.isEmpty()) {
                 System.out.println("Java语法不合法");
                 return false;
             }
             return true;
-        }
-        else{
+        } else {
             System.out.println("Java语法不合法");
             return false;
         }
     }
+
     public static void main(String[] args) throws Exception {
         Exp3_1 exp3_1 = new Exp3_1();
         String exp_String1 = "a=a+b*(c+a)";
         String exp_String2 = "a=(b+c/(d*e)*f";
-        if(exp3_1.isLegal(exp_String1)){
+        if (exp3_1.isLegal(exp_String1)) {
             System.out.println("合法");
-        }else{
+        } else {
             System.out.println("不合法");
         }
-        if(exp3_1.isLegal(exp_String2)){
+        if (exp3_1.isLegal(exp_String2)) {
             System.out.println("合法");
-        }else{
+        } else {
             System.out.println("不合法");
         }
     }
--- workspace_ds/src/com/company/ch3/queue/CircleSqQueueTest.java
+++ workspace_ds/src/com/company/ch3/queue/CircleSqQueueTest.java
@@ -11,19 +11,19 @@ public class CircleSqQueueTest {
         sqStack.offer(3);
         System.out.println("----------插入操作：结束----------");
 
-        System.out.println("----------查看栈顶操作：开始----------");
+        System.out.println("----------查看队列操作：开始----------");
         System.out.println(sqStack.peek());
         System.out.println("弹出栈顶元素："+sqStack.poll());
         System.out.println(sqStack.peek());
         System.out.println("弹出栈顶元素："+sqStack.poll());
-        System.out.println("----------查看栈顶操作：结束----------");
+        System.out.println("----------查看队列操作：结束----------");
 
-        System.out.println("----------查看栈长度操作：开始----------");
+        System.out.println("----------查看队列长度操作：开始----------");
         System.out.println("长度为："+sqStack.length());
-        System.out.println("----------查看栈长度操作：结束----------");
+        System.out.println("----------查看队列长度操作：结束----------");
 
-        System.out.println("----------清除栈操作：开始----------");
+        System.out.println("----------清除队列操作：开始----------");
         sqStack.clear();
-        System.out.println("----------清除栈操作：结束----------");
+        System.out.println("----------清除队列操作：结束----------");
     }
 }
