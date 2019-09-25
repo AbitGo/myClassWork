6871604848ec18d5a80a3b12350558800edaaa6a
AbitGo <15695203200@163.com>
2019-09-25 22:37:41 +0800

    20190925_2
    20190925第二次提交

---
+++ workspace_ds/src/com/company/ch3/queue/LinkSqeue.java
@@ -0,0 +1,83 @@
+package com.company.ch3.queue;
+
+import com.company.ch2.LinkTable.Node;
+import com.company.ch3.IQueue;
+
+public class LinkSqeue implements IQueue {
+    private Node front;
+    private Node rear;
+
+    public LinkSqeue() {
+        this.front = this.rear = null;
+    }
+
+    @Override
+    public void clear() {
+        this.front = this.rear = null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (this.front == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int length() {
+        Node p = this.front;
+        int len = 0;
+        while (p != null) {
+            len++;
+            p = p.next;
+        }
+        return len;
+    }
+
+    //取队内首元素
+    @Override
+    public Object peek() {
+        if (this.isEmpty() == true) {
+            System.out.println("队列为空");
+            return null;
+        } else {
+            return this.front.data;
+        }
+    }
+
+    //入队
+    @Override
+    public void offer(Object x) {
+        Node p = new Node(x);
+        if(this.isEmpty()==true){
+            this.front = this.rear =p;
+        }else{
+            this.rear.next = p;
+            this.rear = p;
+        }
+    }
+
+    //出队
+    @Override
+    public Object poll() {
+        if(this.isEmpty()==true){
+            System.out.println("队列为空");
+            return null;
+        }else{
+            Object p = this.front.data;
+            this.front = this.front.next;
+            return p;
+        }
+    }
+
+    public void display(){
+        Node p = this.front;
+        while (p!=null){
+            System.out.print(" "+p.data);
+            p = p.next;
+        }
+        System.out.println();
+    }
+}
