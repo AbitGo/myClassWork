570876e28187f2fe74a5cf9cd8c4da7f28eafa6d
AbitGo <15695203200@163.com>
2019-10-07 21:35:25 +0800

    20191007_1
    20191007第一次提交

--- workspace_ds/src/com/company/ch4/IString.java
+++ workspace_ds/src/com/company/ch4/IString.java
@@ -4,9 +4,9 @@ public interface IString{
     public void clear();
     public boolean isEmpty();
     public int length();
-    public char charAt(int index);
+    public Object charAt(int index);
     public IString subString(int begin,int end);
     public IString insert(int offset,IString str);
     public IString concat(IString str);
-    public int indxOf(IString str,int begin);
+    public int indexOf(IString str,int begin);
 }
---
+++ workspace_ds/src/com/company/ch4/SequenString/SeqString.java
@@ -0,0 +1,168 @@
+package com.company.ch4.SequenString;
+
+import com.company.ch3.IQueue;
+import com.company.ch4.IString;
+
+public class SeqString implements IString {
+    private char[] strValue;
+    private int curLen;
+
+    public SeqString() {
+        this.curLen = 0;
+        strValue = new char[0];
+    }
+
+    public SeqString(String str) {
+        char[] tempString = str.toCharArray();
+        strValue = new char[str.length()];
+        for (char a : tempString) {
+            strValue[this.curLen] = a;
+            this.curLen++;
+        }
+    }
+
+    public SeqString(char[] str) {
+        strValue = new char[str.length];
+        for (char a : str) {
+            strValue[this.curLen] = a;
+            this.curLen++;
+        }
+    }
+
+    @Override
+    public void clear() {
+        this.curLen = 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (this.curLen == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int length() {
+        return curLen;
+    }
+
+    @Override
+    public Object charAt(int index) {
+        if (this.isEmpty()) {
+            System.out.println("此字符串为空");
+            return null;
+        } else {
+            if (index >= curLen || index < 0) {
+                System.out.println("位置不合法");
+                return null;
+            }
+        }
+        return strValue[index];
+    }
+
+    @Override
+    public IString subString(int begin, int end) {
+        if (this.isEmpty()) {
+            System.out.println("此字符串为空");
+            return null;
+        } else {
+            if (begin < 0 || end < 0 || end < begin || end >= curLen || begin >= curLen) {
+                System.out.println("位置不合法");
+                return null;
+            } else if(begin==end){
+                return this;
+            } else{
+                char[] result = new char[end-begin];
+                for (int i = begin; i < end; i++) {
+                    result[i] = this.strValue[i+begin];
+                }
+                return new SeqString(result);
+            }
+        }
+    }
+
+    @Override
+    public IString insert(int offset, IString str) {
+        if(offset<0||offset>this.curLen){
+            System.out.println("插入位置不正确");
+            return null;
+        }
+        int len = str.length();
+        int needLen = len+this.curLen;
+        if(needLen>strValue.length){
+            alloacte(needLen);
+        }
+
+        //从offset向后移动len长度
+        for(int i=this.curLen-1;i>=offset;i--){
+            strValue[len+i]=strValue[i];
+        }
+        for(int i =0;i<len;i++) {
+            strValue[offset + i] = (char) str.charAt(i);
+        }
+        this.curLen = needLen;
+        return this;
+    }
+
+    public IString delete(int begin,int end){
+        if(begin<0||end>this.curLen || begin>end){
+            System.out.println("删除位置不正确");
+            return null;
+        }
+
+        for(int i=0;i<curLen-end;i++){
+            strValue[begin+i] = strValue[end+i];
+        }
+
+//        int temp_start = begin;
+//        for(int i=end;i<curLen;i++){
+//            strValue[temp_start++] = strValue[i];
+//        }
+        curLen = curLen-(end-begin);
+        return this;
+    }
+
+
+    public void alloacte(int newCapacity){
+        char[] temp = strValue;
+        strValue = new char[newCapacity];
+        for(int i =0;i<temp.length;i++){
+            strValue[i] = temp[i];
+        }
+    }
+
+    public int compareTo(SeqString string){
+        int len1 = curLen;
+        int len2 = string.curLen;
+        int n = Math.min(len1,len2);
+        char[] s1 = strValue;
+        char[] s2 = string.strValue;
+        int k = 0;
+        while (k<n){
+            char ch1 = s1[k];
+            char ch2 = s2[k];
+            if(ch1!=ch2){
+                return ch1-ch2;
+            }
+            k++;
+        }
+        return len1-len2;
+    }
+    @Override
+    public IString concat(IString str) {
+        return insert(curLen,str);
+    }
+
+    @Override
+    public int indexOf(IString str, int begin) {
+        return 0;
+    }
+
+    public void disPlay(){
+        for(char x:strValue){
+            System.out.print(x+" ");
+        }
+    }
+}
