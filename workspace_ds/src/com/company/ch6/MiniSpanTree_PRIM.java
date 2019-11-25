54722162f0171c47198244cedff1a1ea4e3d4a66
AbitGo <15695203200@163.com>
2019-11-25 21:37:58 +0800

    20191125
    20191125

--- workspace_ds/src/com/company/ch6/MiniSpanTree_PRIM.java
+++ workspace_ds/src/com/company/ch6/MiniSpanTree_PRIM.java
@@ -15,22 +15,35 @@ public class MiniSpanTree_PRIM {
     }
 
     public Object[][] PRIM(MGraph g,Object u){
+        //n个顶点最多生成n-1条边
         Object[][] tree = new Object[g.getVexNum()-1][2];
+        //边的个数
         int count = 0;
+        //任意一边到其他点的值
         CloseEdge[] closeEdges = new CloseEdge[g.getVexNum()];
+        //定位位置，初始位置
         int k = g.locateVex(u);
+        //进行分别赋值，一共是n-1条边
         for(int j = 0;j< g.getVexNum();j++){
+            //当自己到自己的时候不需要进行赋值
             if(j!=k){
                 closeEdges[j]=new CloseEdge(u,g.getArcs()[k][j]);
             }
         }
+        //自己到自己是0，与上面进行了呼应
         closeEdges[k]=new CloseEdge(u,0);
-        for(int i = 0;i<g.getVexNum();i++){
-            System.out.println("getVexNum:"+i+"   len:"+closeEdges.length);
+        //选择其余的n-1条边
+        //一定要是从1->n-1或者0->n-2
+        for(int i = 1;i<g.getVexNum();i++){
+            //求出该点到其他边权值最小的一个边
             k = getMiniMun(closeEdges);
+            //进行启示位置的赋值
             tree[count][0] = closeEdges[k].adjVex;
+            //对权值的大小进行赋值
             tree[count][1] = g.getVexs()[k];
+            //边的个数+1
             count++;
+            //对刚才进行过赋值的边进行赋值以免造成影响
             closeEdges[k].lowCost = 0;
             for(int j = 0;j<g.getVexNum();j++){
                 if(g.getArcs()[k][j] <closeEdges[j].lowCost){
