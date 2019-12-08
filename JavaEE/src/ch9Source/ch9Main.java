291e1f79abafd3c19223d00949d34f9079d3dd4c
AbitGo <15695203200@163.com>
2019-12-08 19:17:51 +0800

    20191208
    20191208

--- JavaEE/src/ch9Source/ch9Main.java
+++ JavaEE/src/ch9Source/ch9Main.java
@@ -5,8 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ch9Main {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
-        JuiceMaker2 juiceMaker2 = (JuiceMaker2)applicationContext.getBean("juicemaker2");
-        System.out.println(juiceMaker2.makeJuice());
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+//        JuiceMaker2 juiceMaker2 = (JuiceMaker2)applicationContext.getBean("juicemaker2");
+//        System.out.println(juiceMaker2.makeJuice());
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        JuiceMaker3 juiceMaker3 = (JuiceMaker3)applicationContext.getBean("juicemaker3");
+        System.out.println(juiceMaker3.makeJuice());
+        applicationContext.close();
     }
 }
