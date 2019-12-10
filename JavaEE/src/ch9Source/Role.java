bf0f908e7db344980bda832b6540634a755b54d6
AbitGo <15695203200@163.com>
2019-12-10 20:22:17 +0800

    20191210
    20191210

---
+++ JavaEE/src/ch9Source/Role.java
@@ -0,0 +1,43 @@
+package ch9Source;
+
+public class Role {
+    private long id;
+    private String RoleName;
+    private String note;
+
+    public Role() {
+    }
+
+    public Role(String roleName, String note) {
+        this.RoleName = roleName;
+        this.note = note;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return RoleName;
+    }
+
+    public void setRoleName(String roleName) {
+        RoleName = roleName;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String printInfo(){
+        return "id:"+this.id+" RoleName: "+this.RoleName+" note: "+this.note;
+    }
+}
