8c0de2e5820c43bb09f244b74b015363bf88d417
AbitGo <15695203200@163.com>
2020-01-10 21:36:51 +0800

    20200110
    20200110

--- JavaEE/src/ch10Source/AnnotionMain.java
+++ JavaEE/src/ch10Source/AnnotionMain.java
@@ -7,6 +7,7 @@ public class AnnotionMain {
     public static void main(String[] args) {
         //通过注释去注册java实体类
         ApplicationContext context = new AnnotationConfigApplicationContext(PoJoConfig.class);
+        //需要将RoleAnnotation类注入进去
         RoleAnnotation role = context.getBean(RoleAnnotation.class);
         System.out.println(role.getId());
     }
