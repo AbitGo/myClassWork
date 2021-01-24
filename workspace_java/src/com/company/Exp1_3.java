c4bb240b54424ec199cffc172617c831b54b6024
韦海涛 <15695203200@163.com>
2021-01-24 11:07:29 +0800

    20200124_4
    20200124_4

--- workspace_java/src/com/company/Exp1_3.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Exp1_3 {
-    public static void main(String[] args) {
-        System.out.println("******************");
-        System.out.println("Welcome");
-        System.out.println("******************");
-    }
-}
